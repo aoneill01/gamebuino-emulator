c114b8d51df908857fcbe43b7303c40ff6e8cdbc
Andy O'Neill <aoneill@measinc.com>
2017-11-10 16:34:02 -0500

    Initial webpack.

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,5 @@
-export { decode } from './intel-hex'
+import { Atsamd21 } from './atsamd21';
+
+console.log('hello');
+var atsamd21 = new Atsamd21();
+document.getElementById('light').innerHTML = atsamd21.pcIndex.toString(16);
