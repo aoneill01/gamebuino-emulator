b94211074f3198a9a48d43cba0de043d3ca83bcc
Andy O'Neill <aoneill01@gmail.com>
2017-11-25 12:33:15 -0500

    Testing blink.

--- src/atsamd21.spec.ts
+++ src/atsamd21.spec.ts
@@ -10,10 +10,14 @@ fs.readFile('./Blink.ino.arduino_zero.bin', function(err, data) {
 
     var micro = new Atsamd21();
     micro.loadFlash(memory, 0x2000);
-    var count = 48000000;
+    micro.portA.addOutListener((mask: number, value: number) => {
+        console.log(`mask: ${mask.toString(2)}; value: ${value.toString(2)}`);
+    });
+    var count = 480000000;
     for (var i = 0; i < count; i++) {
         micro.step();
     }
+    console.log("no crash");
 });
 
 /*
